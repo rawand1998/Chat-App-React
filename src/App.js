76f253730c8c82a205b154e9baab4550b03e9aad
rawand1998 <rawandgaradh@gmail.com>
2022-07-13 14:36:27 +0300

    login with google provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React,{useEffect, useState} from 'react'
-import Title from './Components/Title'
-import MassageList from './Components/MassageList'
-import SendMassageForm from './Components/SendMassageForm'
-
-
+import React from 'react'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {auth} from './firebase'
 import './App.css'
+import Login from './Components/Login'
+import Chat from './Components/Chat'
 function App() {
+  const [user] = useAuthState(auth)
  return (
    <div>
-     <Title />
-     <MassageList />
-     <SendMassageForm />
+       {user ? <Chat /> : <Login />}
    </div>
  )
 }
