83926d0175acc1baaf597d1a2652cfb7f8d7e8c9
rawand1998 <rawandgaradh@gmail.com>
2022-07-13 14:59:41 +0300

    get massage

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Logout from './Logout'
 
+import {db} from '../firebase'
+
 function Chat() {
+    const [msg,setMsg] = useState([])
+    
+    useEffect(()=>{
+     const getMsg = async () => {
+
+db.collection('massage').onSnapshot((snapshot) => {
+    setMsg(snapshot.docs.map((doc) =>doc.data()))
+})
+      }
+      getMsg()
+    },[])
   return (
   
     <div>
           <Logout />
-          chat
+          <div>
+           {msg.map((msg) =>(
+            <h1>{msg.text}</h1>
+           ))}
+          </div>
+      
     </div>
   )
 }
